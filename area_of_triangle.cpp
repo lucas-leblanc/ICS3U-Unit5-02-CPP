d739abf9ae066c9100ff01e951098d1dfb47d640
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-12-01 04:31:53 +0000

    added file

---
+++ area_of_triangle.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2022 Lucas LeBlanc All rights reserved
+
+// Created by: Lucas LeBlanc
+// Created on: Nov 2022
+// This program calculates area of triangle using functions
+
+#include <iostream>
+
+void Area(int base, int height) {
+    // This function calculates the area of the triangle
+    int area;
+
+    // Process
+    area = base * height / 2;
+
+    // Output
+    std::cout << "\nThe area of the triangle is " << area << " cm²."
+              << std::endl;
+}
+
+int main() {
+    // This function gets user inputs and does try catch
+    std::string base_as_string;
+    std::string height_as_string;
+    int base;
+    int height;
+
+    // Input
+    std::cout << "Enter the base length of the triangle (cm): ";
+    std::cin >> base_as_string;
+    std::cout << "Enter the height of the triangle (cm): ";
+    std::cin >> height_as_string;
+
+    // try and catch
+    try {
+        base = std::stoi(base_as_string);
+        height = std::stoi(height_as_string);
+        // Call function
+        Area(base, height);
+    } catch (std::invalid_argument) {
+        std::cout << "\nInvalid Input." << std::endl;
+    }
+
+    std::cout << "\nDone." << std::endl;
+}
